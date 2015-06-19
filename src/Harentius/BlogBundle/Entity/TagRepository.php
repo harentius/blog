f7af1ec61098a4fde458f6e182d9cbbd89a82e3c
Yaroslav Osypenko <harentius@yandex.ru>
2015-06-20 01:20:56 +0300

    added repository method for fetching tags

--- src/Harentius/BlogBundle/Entity/TagRepository.php
+++ src/Harentius/BlogBundle/Entity/TagRepository.php
@@ -3,8 +3,33 @@
 namespace Harentius\BlogBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class TagRepository extends EntityRepository
 {
+    /**
+     * @param $limit
+     * @return mixed
+     */
+    public function findMostPopularLimited($limit)
+    {
+        $tags = $this->createQueryBuilder('t')
+            ->select('t.name, t.slug, COUNT(a) as weight')
+            ->join('t.articles', 'a')
+            ->orderBy('weight', 'DESC')
+            ->groupBy('t.slug')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->setHydrationMode(Query::HYDRATE_ARRAY)
+            ->execute()
+        ;
 
+        $result = [];
+
+        foreach ($tags as $tag) {
+            $result[] = $tag;
+        }
+
+        return $result;
+    }
 }
