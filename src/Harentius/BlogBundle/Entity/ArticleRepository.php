4e9ab755b8d9c80b99aebfc4f2f3438fc2cc054a
Yaroslav Osypenko <harentius@yandex.ru>
2015-06-20 02:32:41 +0300

    added repository methods for selecting articles by tags and categories

--- src/Harentius/BlogBundle/Entity/ArticleRepository.php
+++ src/Harentius/BlogBundle/Entity/ArticleRepository.php
@@ -6,5 +6,33 @@ use Doctrine\ORM\EntityRepository;
 
 class ArticleRepository extends EntityRepository
 {
+    /**
+     * @param string $categorySlug
+     * @return mixed
+     */
+    public function findOneByCategorySlug($categorySlug)
+    {
+        return $this->createQueryBuilder('a')
+            ->join('a.category', 'c')
+            ->where('c.slug = :categorySlug')
+            ->setParameter(':categorySlug', $categorySlug)
+            ->getQuery()
+            ->execute()
+        ;
+    }
 
+    /**
+     * @param string $tagSlug
+     * @return mixed
+     */
+    public function findByTagSlug($tagSlug)
+    {
+        return $this->createQueryBuilder('a')
+            ->join('a.tags', 't')
+            ->where('t.slug = :tagSlug')
+            ->setParameter(':tagSlug', $tagSlug)
+            ->getQuery()
+            ->execute()
+        ;
+    }
 }
