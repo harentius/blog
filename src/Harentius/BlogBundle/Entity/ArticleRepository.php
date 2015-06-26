f2f78c1aa97c594d4657a6c5d1a169010b7399df
Yaroslav Osypenko <harentius@yandex.ru>
2015-06-27 01:49:15 +0300

    created repository method for selecting articles

--- src/Harentius/BlogBundle/Entity/ArticleRepository.php
+++ src/Harentius/BlogBundle/Entity/ArticleRepository.php
@@ -7,6 +7,30 @@ use Doctrine\ORM\EntityRepository;
 class ArticleRepository extends EntityRepository
 {
     /**
+     * @param Category $category
+     * @return mixed
+     */
+    public function findByCategory(Category $category)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        return $qb
+            ->join('a.category', 'ca')
+            ->where('a.category IN
+                (SELECT c FROM HarentiusBlogBundle:Category c
+                 WHERE c.left >= :left AND c.right <= :right AND c.root = :root)'
+            )
+            ->setParameters([
+                ':left' => $category->getLeft(),
+                ':right' => $category->getRight(),
+                ':root' => $category->getRoot(),
+            ])
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
+    /**
      * @param Tag $tag
      * @return mixed
      */
