3c492571f2f5347dbd44e7536e2a0d7b1db0deb7
Yaroslav Osypenko <harentius@yandex.ru>
2015-07-10 23:20:07 +0300

    added basic admin class for notices

---
+++ src/Harentius/BlogBundle/Admin/ArticleAdmin.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Harentius\BlogBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class ArticleAdmin extends Admin
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('title')
+            ->add('slug')
+            ->add('category')
+            ->add('tags')
+            ->add('author')
+            ->add('isPublished')
+            ->add('publishedAt')
+            ->add('metaDescription')
+            ->add('metaKeywords')
+            ->add('_action', 'actions', [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ]
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('slug')
+            ->add('category')
+            ->add('tags', null, [
+                'attr' => ['class' => 'tags-list'],
+            ])
+            ->add('text', 'textarea', [
+                'attr' => ['rows' => 12, 'class' => 'blog-post-text'],
+            ])
+            ->add('isPublished')
+            ->add('publishedAt', 'datetime')
+            ->add('metaDescription', 'textarea', [
+                'required' => false,
+            ])
+            ->add('metaKeywords', 'text', [
+                'required' => false,
+            ])
+        ;
+    }
+}
