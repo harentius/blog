292406a816f1723f99341c3773ae5df02abfe8d8
Yaroslav Osypenko <harentius@yandex.ru>
2015-08-19 22:33:40 +0300

    fixes for avoiding duplicates

--- src/Harentius/BlogBundle/Command/DatabasePopulateCommand.php
+++ src/Harentius/BlogBundle/Command/DatabasePopulateCommand.php
@@ -28,32 +28,50 @@ class DatabasePopulateCommand extends ContainerAwareCommand
         $encoder = $this->getContainer()->get('security.password_encoder');
         $entitiesForPersisting = [];
 
+        // TODO: Refactor
         // Admin user
-        $adminUser = new AdminUser();
-        $adminUser
-            ->setUsername('admin')
-            ->setPassword($encoder->encodePassword($adminUser, 'admin'))
-        ;
+        if (!$em->getRepository('HarentiusBlogBundle:AdminUser')->findOneBy(['username' => 'admin'])) {
+            $adminUser = new AdminUser();
+            $adminUser
+                ->setUsername('admin')
+                ->setPassword($encoder->encodePassword($adminUser, 'admin'));
 
-        $entitiesForPersisting[] = $adminUser;
+            $entitiesForPersisting[] = $adminUser;
+        }
 
         // Settings
-        $entitiesForPersisting = array_merge($entitiesForPersisting, [
-            (new Setting())
+        if (!$em->getRepository('HarentiusBlogBundle:Setting')->findOneBy(['key' => 'project_name'])) {
+            $setting = new Setting();
+            $setting
                 ->setKey('project_name')
                 ->setName('Project Name')
-                ->setValue('Simple Symfony Blog'),
+                ->setValue('Simple Symfony Blog')
+            ;
 
-            (new Setting())
+            $entitiesForPersisting[] = $setting;
+        }
+
+        if (!$em->getRepository('HarentiusBlogBundle:Setting')->findOneBy(['key' => 'homepage_meta_description'])) {
+            $setting = new Setting();
+            $setting
                 ->setKey('homepage_meta_description')
                 ->setName('Homepage Meta Description')
-                ->setValue('Simple Symfony Blog description'),
+                ->setValue('Simple Symfony Blog description')
+            ;
 
-            (new Setting())
+            $entitiesForPersisting[] = $setting;
+        }
+
+        if (!$em->getRepository('HarentiusBlogBundle:Setting')->findOneBy(['key' => 'homepage_meta_keywords'])) {
+            $setting = new Setting();
+            $setting
                 ->setKey('homepage_meta_keywords')
                 ->setName('Homepage Meta Keywords')
-                ->setValue('symfony blog'),
-        ]);
+                ->setValue('symfony blog')
+            ;
+
+            $entitiesForPersisting[] = $setting;
+        }
 
         foreach ($entitiesForPersisting as $entity) {
             $em->persist($entity);
