8d88295b183672b8ecb3718a3f0a0ae86949dbcd
Yaroslav Osypenko <harentius@yandex.ru>
2016-06-26 23:06:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Harentius blog
+==============
+
+
+Project on the top of [https://github.com/harentius/folkprog](https://github.com/harentius/blog-bundle)
+
+For quick launching this blog/cms as a standalone blog/portal project.
+
+For integrating harentius/blog-bundle in existing symfony project please read appropriate doc: [https://github.com/harentius/folkprog](https://github.com/harentius/blog-bundle)
+
+
+Installation:
+-------------
+
+1)
+```bash
+$ composer.phar create-project harentius/blog-bundle
+```
+
+2) bin/install
---
+++ app/AppKernel.php
@@ -0,0 +1,51 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\TranslationBundle\SonataTranslationBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Presta\SitemapBundle\PrestaSitemapBundle(),
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+            new Eko\FeedBundle\EkoFeedBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle(),
+            new Harentius\BlogBundle\HarentiusBlogBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
