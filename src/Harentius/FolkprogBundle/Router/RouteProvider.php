d4a95291983e5a825286b229551087c0cbbe3438
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-01-14 22:43:50 +0200

    fix

--- src/Harentius/FolkprogBundle/Router/RouteProvider.php
+++ src/Harentius/FolkprogBundle/Router/RouteProvider.php
@@ -2,6 +2,7 @@
 
 namespace Harentius\FolkprogBundle\Router;
 
+use Harentius\BlogBundle\Router\CategorySlugProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
 use Harentius\BlogBundle\Router\RouteProvider as BaseRouteProvider;
@@ -9,18 +10,34 @@ use Harentius\BlogBundle\Router\RouteProvider as BaseRouteProvider;
 class RouteProvider extends BaseRouteProvider
 {
     /**
+     * @var ArticleAndPageSlugRequirementProvider
+     */
+    private $articleAndPageSlugRequirementProvider;
+
+    /**
+     * @param CategorySlugProvider $categorySlugProvider
+     * @param ArticleAndPageSlugRequirementProvider $articleAndPageSlugRequirementProvider
+     */
+    public function __construct(
+        CategorySlugProvider $categorySlugProvider,
+        ArticleAndPageSlugRequirementProvider $articleAndPageSlugRequirementProvider
+    ) {
+        parent::__construct($categorySlugProvider);
+        $this->articleAndPageSlugRequirementProvider = $articleAndPageSlugRequirementProvider;
+    }
+
+    /**
      * @inheritdoc
      */
     public function getRouteCollectionForRequest(Request $request)
     {
         parent::getRouteCollectionForRequest($request);
-        $slugProvider = $this->container->get('harentius_folkprog.router.article_and_page_slug_requirement_provider');
 
         $this->routes->add("harentius_blog_show_en", new Route(
             "{_locale}/{slug}/",
             ['_controller' => 'HarentiusBlogBundle:Blog:show', '_locale' => 'en'],
             [
-                'slug' => $slugProvider->getRequirement(),
+                'slug' => $this->articleAndPageSlugRequirementProvider->getRequirement(),
                 '_locale' => 'en',
             ]
         ));
