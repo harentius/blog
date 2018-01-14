0dc8d6a2cf5cea1338ac3cdd6d707cfa71361433
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-01-14 23:34:45 +0200

    optimize front controller

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,17 +1,11 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
+require __DIR__.'/../vendor/autoload.php';
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
-
 $kernel = new AppKernel('dev', true);
 
 $request = Request::createFromGlobals();
