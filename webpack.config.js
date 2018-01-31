d8feba3d78105b6e2846bccf491ac387c8369e97
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-01-31 22:10:46 +0200

    corrections

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const Encore = require('@symfony/webpack-encore');
 
 Encore
-  .setOutputPath('src/Harentius/FolkprogBundle/Resources/public/js/build/')
-  .setPublicPath('/bundles/harentiusfolkprog/')
-  .setManifestKeyPrefix('bundles/harentiusfolkprog/')
+  .setOutputPath('web/assets/build/')
+  .setPublicPath('/assets/build/')
+  .setManifestKeyPrefix('assets/build/')
   .cleanupOutputBeforeBuild()
   .addEntry('common-folkprog', './src/Harentius/FolkprogBundle/Resources/js/common-folkprog.js')
   .addStyleEntry('main', './src/Harentius/FolkprogBundle/Resources/css/main.less')
