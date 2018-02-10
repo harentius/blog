36636cef64114133a2d854283869ff1febe7882e
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-02-11 00:03:09 +0200

    switch to dot env

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,9 +1,20 @@
 <?php
 
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
+
+$filesPaths = [__DIR__.'/../.env.dist'];
+$overwrittenDotEnvFilePath = __DIR__.'/../.env';
+
+if (is_readable($overwrittenDotEnvFilePath)) {
+    $filesPaths[] = $overwrittenDotEnvFilePath;
+}
+
+$dotenv = new Dotenv();
+$dotenv->load(...$filesPaths);
 
 Debug::enable();
 $kernel = new AppKernel('dev', true);
