8009c27425441fd5d092d0ddcb9723c0ac323a9d
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-02-11 00:22:21 +0200

    read dot env in test mode

--- web/app_test.php
+++ web/app_test.php
@@ -1,10 +1,21 @@
 <?php
 
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
 umask(0000);
 require __DIR__.'/../vendor/autoload.php';
+
+$filesPaths = [__DIR__.'/../.env.dist'];
+$overwrittenDotEnvFilePath = __DIR__.'/../.env';
+
+if (is_readable($overwrittenDotEnvFilePath)) {
+    $filesPaths[] = $overwrittenDotEnvFilePath;
+}
+
+$dotenv = new Dotenv();
+$dotenv->load(...$filesPaths);
 
 Debug::enable();
 $kernel = new AppKernel('test', false);
