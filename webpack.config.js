d8235875866b60c2e90e091d85f105decfe5352a
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-03-03 20:09:44 +0200

    update adsence code

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ Encore
   .setPublicPath('/build/')
   .setManifestKeyPrefix('build/')
   .cleanupOutputBeforeBuild()
-  .addEntry('common-folkprog', './src/Harentius/FolkprogBundle/Resources/js/common-folkprog.js')
   .addStyleEntry('main', './src/Harentius/FolkprogBundle/Resources/css/main.less')
   .enableLessLoader()
   .enableVersioning()
