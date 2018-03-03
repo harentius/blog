8ec67782cff96bcd20b11e47208013266a681efb
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-03-03 20:19:30 +0200

    Revert "update adsence code"
    This reverts commit d8235875866b60c2e90e091d85f105decfe5352a.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ Encore
   .setPublicPath('/build/')
   .setManifestKeyPrefix('build/')
   .cleanupOutputBeforeBuild()
+  .addEntry('common-folkprog', './src/Harentius/FolkprogBundle/Resources/js/common-folkprog.js')
   .addStyleEntry('main', './src/Harentius/FolkprogBundle/Resources/css/main.less')
   .enableLessLoader()
   .enableVersioning()
